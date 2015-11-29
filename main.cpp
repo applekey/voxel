2e5106c108691e269382facb1228d889eb1a647d
vincent <applekey@gmail.com>
2015-11-29 14:16:23 -0800

    fdsa

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <sstream>
 #include <cstdlib>
+#include <float.h>
 #include </usr/local/include/glm/vec3.hpp> // glm::vec3
 #include </usr/local/include/glm/vec4.hpp> // glm::vec4
 #include </usr/local/include/glm/mat4x4.hpp> // glm::mat4
@@ -14,9 +15,9 @@
 #include "tbb/blocked_range.h"
 
 #define OBJFILE "/Users/applekey/Documents/datasets/monkey.obj"
+#define SCREENSIZE 64
 
-
-std::vector<glm::vec3> LoadObj(const char * filename)
+std::vector<glm::vec3> LoadObjFindAABB(const char * filename, glm::vec3 * aabbLow, glm::vec3 *  aabbHigh)
 {
     std::ifstream in(filename, std::ios::in);
     if (!in)
@@ -40,6 +41,40 @@ std::vector<glm::vec3> LoadObj(const char * filename)
             double x,y,z;
             v>>x;v>>y;v>>z;
             vert=glm::vec3(x,y,z);
+            
+            // x
+            if ( x < aabbLow->x)
+            {
+                aabbLow->x = x;
+            }
+            
+            if (x > aabbHigh->x)
+            {
+                aabbHigh->x = x;
+            }
+            
+            // y
+            if ( y < aabbLow->y)
+            {
+                aabbLow->y = y;
+            }
+            
+            if (y > aabbHigh->y)
+            {
+                aabbHigh->y = y;
+            }
+            
+            // z
+            if ( z < aabbLow->z)
+            {
+                aabbLow->z = z;
+            }
+            
+            if (z > aabbHigh->z)
+            {
+                aabbHigh->z = z;
+            }
+            
             vertices.push_back(vert);
         }
         else if(line.substr(0,2)=="f "){
@@ -63,31 +98,125 @@ std::vector<glm::vec3> LoadObj(const char * filename)
     return meshVertices;
 }
 
-//void voxelOperator(unsigned int triId, const std::vector<glm::vec3> triangles, unsigned char * storage)
-//{
-//    unsigned int triangleVertId = triId * 3;
-//}
+int isPowerOfTwo (unsigned int x)
+{
+    while (((x & 1) == 0) && x > 1) /* While x is even and > 1 */
+        x >>= 1;
+    return (x == 1);
+}
 
+/*------------------------------main functions --------------------*/
 struct voxelOperator {
-    const std::vector<glm::vec3> triangles;
+    const glm::vec3 * voxelSize;
+    std::vector<glm::vec3> * triangles;
     unsigned char * storage;
+    glm::vec3 aabbLow;
+    glm::vec3 aabbHigh;
+    float aabbXRun;
+    float aabbYRun;
+    float aabbZRun;
+    
+    struct aabb
+    {
+        glm::vec3 lower;
+        glm::vec3 upper;
+    };
+    
+    struct aabb calculateBoundingBox(glm::vec3 vert1, glm::vec3 vert2, glm::vec3 vert3) const
+    {
+        struct aabb cd;
+        
+        cd.lower.x = fmin(vert1.x,(fmin(vert2.x, vert3.x)));
+        cd.lower.y = fmin(vert1.y,(fmin(vert2.y, vert3.z)));
+        cd.lower.z = fmin(vert1.z,(fmin(vert2.y, vert3.z)));
+        
+        cd.upper.x = fmax(vert1.x,(fmax(vert2.x, vert3.x)));
+        cd.upper.y = fmax(vert1.y,(fmax(vert2.y, vert3.z)));
+        cd.upper.z = fmax(vert1.z,(fmax(vert2.y, vert3.z)));
+        
+        return cd;
+    }
+    
+    int min(int x, int y) const
+    {
+        return  y + ((x - y) & ((x - y) >>
+                                (sizeof(int) * CHAR_BIT - 1)));
+    }
+    
+    int largestAxis(float x, float y, float z) const
+    {
+        return fmin(fabs(x), fmax(fabs(y), fabs(z)));
+    }
+    
+    
+    void TriangleRasterizer(glm::vec3 vert1,glm::vec3 vert2,glm::vec3 vert3, unsigned char * output) const
+    {
+        //calculate smaller bounding box
+        
+        struct aabb triaabb = calculateBoundingBox(vert1,vert2,vert3);
+        // conver to screen space
+        
+        
+        
+    }
+    
+    
     void operator()( const tbb::blocked_range<int>& range ) const {
         for( int i=range.begin(); i!=range.end(); ++i )
         {
+            glm::vec3 * verts = &(*triangles)[0];
             unsigned int triangleVertId = i * 3;
+            //verticies
+            glm::vec3 vert1 = verts[triangleVertId];
+            glm::vec3 vert2 = verts[triangleVertId + 1];
+            glm::vec3 vert3 = verts[triangleVertId + 2];
+            
+            TriangleRasterizer(vert1,vert2,vert3,storage);
+            
+            // find the max area
+//            glm::vec3 normal = glm::normalize(glm::cross(vert3 - vert1, vert2 - vert1));
+//            int projectionAxis = largestAxis(normal.x,normal.y,normal.z);
+            
+            //rasterize the triangle
+            
+            
+            
+            
+            
         }
     }
 };
-
+/*------------------------------main functions end --------------------*/
 
 int main()
 {
+    //screen size
+    glm::vec3 screenSize(SCREENSIZE,SCREENSIZE,SCREENSIZE);
+    
+    //intilize output
+    assert(isPowerOfTwo(SCREENSIZE));
+    unsigned char * output = new unsigned char [SCREENSIZE * SCREENSIZE * SCREENSIZE / 8];
+    
+    //AABB
+    glm::vec3 aabbLow = glm::vec3(FLT_MIN,FLT_MIN,FLT_MIN);
+    glm::vec3 aabbHigh = glm::vec3(FLT_MAX,FLT_MAX,FLT_MAX);
+    
     //read in obj
-    std::vector<glm::vec3> triangles = LoadObj(OBJFILE);
-    voxelOperator vop;
+    std::vector<glm::vec3> triangles = LoadObjFindAABB(OBJFILE, &aabbLow,&aabbHigh);
+    struct voxelOperator vop;
+    vop.triangles = &triangles;
+    vop.voxelSize = &screenSize;
+    vop.storage = output;
+    vop.aabbLow = aabbLow;
+    vop.aabbHigh = aabbHigh;
+    vop.aabbXRun = aabbHigh.x - aabbLow.x;
+    vop.aabbYRun = aabbHigh.y - aabbLow.y;
+    vop.aabbZRun = aabbHigh.z - aabbLow.z;
     
     tbb::parallel_for( tbb::blocked_range<int>( 1, triangles.size()/3 ), vop );
     
     
+    
+    return 0;
     
 }
